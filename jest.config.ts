f1ddbb6f151066d0fd9ef4fa71a905226437033d
JoaoFerrazfs <joaoferrazp@gmail.com>
2025-05-01 15:31:17 -0300

    tests: rewrite coverage config file

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.{ts,js}',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+
+};
+
+export default config;
